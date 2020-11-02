9f8e5978ccadfbca82671d060c7fbd8a22630701
eldiablo-1226 <shaxzod12262000@gmail.com>
2020-11-02 17:38:27 +0500

    create BaseViewModel

---
+++ src/Youtube-DL/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Youtube_DL.Core;
+
+namespace Youtube_DL.ViewModel
+{
+    class MainViewModel : BaseViewModel
+    {
+
+    }
+}
