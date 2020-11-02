b6767df1a014c08c191c5d08001d55246cc2d88a
eldiablo-1226 <shaxzod12262000@gmail.com>
2020-11-03 00:29:24 +0500

    0.3

--- src/Youtube-DL/View/MainView.xaml.cs
+++ src/Youtube-DL/View/MainView.xaml.cs
@@ -1,26 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using Youtube_DL.ViewModel;
 
 namespace Youtube_DL.View
 {
-    /// <summary>
-    /// Логика взаимодействия для MainView.xaml
-    /// </summary>
     public partial class MainView : UserControl
     {
         public MainView()
         {
             InitializeComponent();
+            this.DataContext = new MainViewModel();
+
         }
     }
 }
