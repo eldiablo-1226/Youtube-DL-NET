7041b7ac90b706a7b41e7f394ffd8182e90eac5b
eldiablo-1226 <shaxzod12262000@gmail.com>
2020-11-12 21:45:05 +0500

    1.0.3

--- src/Youtube-DL/ViewModel/AddVideoPopupViewModel.cs
+++ src/Youtube-DL/ViewModel/AddVideoPopupViewModel.cs
@@ -4,7 +4,12 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using MaterialDesignThemes.Wpf;
+using NYoutubeDL;
+using NYoutubeDL.Models;
+using Youtube_DL.Core;
+using Youtube_DL.Model;
 
 namespace Youtube_DL.ViewModel
 {
@@ -12,10 +17,55 @@ namespace Youtube_DL.ViewModel
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        YoutubeDL ydlClient = new YoutubeDL(@"Resources\youtube-dl.exe");
+        public Command AddVideoCommand { get; set; }
         public bool IsLoading { get; set; }
 
         public AddVideoPopupViewModel()
         {
+            AddVideoCommand = new Command(AddVideoToList);
+        }
+
+        private async void AddVideoToList(object s)
+        {
+            if (IsLoading || s == null) return;
+            string url = s as string;
+            if (string.IsNullOrEmpty(url))
+                MessageBox.Show("Пожалуйста введите ссылку !!!");
+            else
+            {
+                if (CheckURL(url))
+                {
+                    try
+                    {
+                        IsLoading = true;
+                        var YI = await ydlClient.GetDownloadInfoAsync(url);
+                        IsLoading = false;
+                        DialogHost.Close("MainDialog", new YoutubeVideoModel(YI, url));
+
+                    }
+                    catch (Exception e)
+                    {
+                        if (IsLoading)
+                            IsLoading = false;
+                        MessageBox.Show(e.Message);
+                    }
+                }
+                else
+                    MessageBox.Show("Ссылку не валидна !!!");
+            }
+        }
+
+        private bool CheckURL(string videoUri)
+        {
+            videoUri = new StringBuilder(videoUri).Replace("youtu.be/", "youtube.com/watch?v=")
+                .Replace("youtube.com/embed/", "youtube.com/watch?v=").Replace("/v/", "/watch?v=")
+                .Replace("/watch#", "/watch?").ToString();
+
+            if (videoUri.Contains("youtube.com/watch?v="))
+                return true;
+            return false;
         }
     }
 }
+
--- src/Youtube-DL/ViewModel/MainViewModel.cs
+++ src/Youtube-DL/ViewModel/MainViewModel.cs
@@ -1,13 +1,7 @@
 ﻿using MaterialDesignThemes.Wpf;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using NYoutubeDL;
 using Youtube_DL.Core;
 using Youtube_DL.Model;
 using Youtube_DL.View;
@@ -16,11 +10,9 @@ namespace Youtube_DL.ViewModel
 {
     internal class MainViewModel : INotifyPropertyChanged
     {
-        YoutubeDL ydlClient = new YoutubeDL(@"Resources\youtube-dl.exe");
         public event PropertyChangedEventHandler PropertyChanged;
 
         #region Property
-        public SnackbarMessageQueue Snackbar { get; set; } = new SnackbarMessageQueue();
 
         public bool Isloading { get; set; }
 
@@ -37,59 +29,16 @@ namespace Youtube_DL.ViewModel
         public MainViewModel()
         {
             MainVideoList = new ObservableCollection<YoutubeVideoModel>();
-            //AddButton = new Command((s) => AddToDownload(s as string));
-            AddButton = new Command(Test);
+            AddButton = new Command(AddViewShow);
         }
 
-        private async void Test()
+        private async void AddViewShow()
         {
-            var valeu = await DialogHost.Show(new AddVideoPopup());
-        }
+            var VideoInfo = await DialogHost.Show(new AddVideoPopup());
 
-        private async void AddToDownload(string urls)
-        {
-            if (string.IsNullOrEmpty(urls))
-                Snackbar.Enqueue("Пожалуйста введите ссылку !!!");
-            else
-            {
-                if (CheckURL(urls))
-                {
-                    try
-                    {
-                        if(MainVideoList.Any(x=> x.URL == urls))
-                            throw new ArgumentException(message:"Это ссылка уже добавлена");
+            if(VideoInfo is null) return;
 
-                        await AddVideo(urls);
-                    }
-                    catch (Exception e)
-                    {
-                        if(Isloading)
-                            Isloading = false;
-                        Snackbar.Enqueue(e.Message);
-                    }
-                }
-                else
-                    Snackbar.Enqueue("Ссылку не валидна !!!");
-            }
-        }
-
-        private async Task AddVideo(string urls)
-        {
-            Isloading = true;
-
-            var YI = await ydlClient.GetDownloadInfoAsync(urls);
-            MainVideoList.Add(new YoutubeVideoModel(YI, urls));
-
-            Isloading = false;
-        }
-
-        private bool CheckURL(string videoUri)
-        {
-            videoUri = new StringBuilder(videoUri).Replace("youtu.be/", "youtube.com/watch?v=").Replace("youtube.com/embed/", "youtube.com/watch?v=").Replace("/v/", "/watch?v=").Replace("/watch#", "/watch?").ToString();
-
-            if (videoUri.Contains("youtube.com/watch?v="))
-                return true;
-            return false;
+            MessageBox.Show(VideoInfo.GetType().FullName);
         }
     }
 }
